87376868af9f8c1d6901ca6cc929f17094680fc6
dahyun <gimd15629@gmail.com>
2025-02-08 10:45:31 +0900

    44:32 배경 수정 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { initMongoose } from "@/lib/mongoose";
 import Product from "../components/Product";
+import Footer from "../components/Footer";
+import Layout from "../components/Layout";
 import Image from "next/image";
 import React from "react";
 import { useEffect, useState } from "react";
@@ -25,26 +27,28 @@ export default function Home() {
   }
 
   return (
-    <div className="p-5">
-      <input value={phrase} onChange={e => setPhrase(e.target.value)} type="text" placeholder="Search for products..." className="bg-gray-100 text-black w-full py-2 px-4 rounded-xl"/>
-      <div>
-        {categoriesNames.map(categoryName => (
-          <div key={categoryName}>
-            {products.find(p => p.category === categoryName) && (
-              <div>
-                <h2 className="text-2xl py-5 capitalize">{categoryName}</h2>
-                <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
-                  {products.filter(p => p.category === categoryName).map(productInfo => (
-                    <div key={productInfo._id} className='px-5 snap-start'>
-                      <Product { ...productInfo} />
-                    </div>
-                  ))}
+    <Layout>
+      <div className="bg-white text-black">
+        <input value={phrase} onChange={e => setPhrase(e.target.value)} type="text" placeholder="Search for products..." className="bg-gray-100 text-black w-full py-2 px-4 rounded-xl"/>
+        <div>
+          {categoriesNames.map(categoryName => (
+            <div key={categoryName}>
+              {products.find(p => p.category === categoryName) && (
+                <div>
+                  <h2 className="text-2xl py-5 capitalize">{categoryName}</h2>
+                  <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
+                    {products.filter(p => p.category === categoryName).map(productInfo => (
+                      <div key={productInfo._id} className='px-5 snap-start'>
+                        <Product { ...productInfo} />
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            )}
-          </div>
-        ))}
+              )}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
